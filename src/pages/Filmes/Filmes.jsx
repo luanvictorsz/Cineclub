d9a884bbb03b80e4223b704c534b652acffc5004
luanvictorsz <luanvitorlima2017@gmail.com>
2025-01-08 01:43:46 -0300

    estrutura da pagina de detalhes

--- src/pages/Filmes/Filmes.jsx
+++ src/pages/Filmes/Filmes.jsx
@@ -1,7 +1,59 @@
+import { useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+
+import api from '../../services/api';
+
 function Filmes(){
+const {id} = useParams();
+const [filme, setFilme] = useState({});
+const [loading, setLoading] = useState(true);
+
+useEffect(() =>{
+    async function loadFilme(){
+        await api.get(`/movie/${id}`, {
+            params: {
+                api_key: "", 
+                language: "pt-BR",
+            }
+        })
+        .then((response) =>{
+            setFilme(response.data);
+            setLoading(false);
+        })
+        .catch(() =>{
+            console.log("filme não encontrado");
+        })
+    }
+
+    loadFilme();
+
+
+    return ()=>{
+        console.log("Componenete foi desmontado")
+    }
+
+}, [])
+
+if(loading){
     return(
-        <h1>Bem Vindo a Pagina de filmes</h1>
-    )
+        <div className='filme-info'>
+            <h1>Carregando Detalhes...</h1>
+        </div>
+    )    
+}
+
+return(
+    <div className='filme-info'>
+        <h1>{filme.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+            alt={filme.title}
+        />
+
+        <h3>Sinopse</h3>
+        <span>{filme.overview}</span>
+        <strong>Avaliação: {filme.vote_average} / 10</strong>
+    </div>
+)
 }
 
 export default Filmes;
