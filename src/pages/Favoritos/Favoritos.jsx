047835fbc622c37c3b9f11415ec63314693016d9
a3ternus <luanvitorlima2017@gmail.com>
2025-01-15 01:48:44 -0300

    adicionando funcionalidade a função de excluir filmes da lista

--- src/pages/Favoritos/Favoritos.jsx
+++ src/pages/Favoritos/Favoritos.jsx
@@ -18,16 +18,27 @@ function Favoritos() {
     }
   }, []); 
 
+  function ExcluirFilme(id){
+    let filtroFilmes = filmes.filter((item) => {
+        return(item.id !== id)
+    })
+
+    setFilmes(filtroFilmes)
+    localStorage.setItem('@cineClube', JSON.stringify(filtroFilmes))
+
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus Favoritos</h1>
+      {filmes.length === 0 && <span>Você não possui filmes salvos</span>}
       <ul>
         {filmes.map((filme) => (
           <li key={filme.id}>
             <span>{filme.title}</span>
             <div>
               <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
-              <button>Excluir</button>
+              <button onClick={() => ExcluirFilme(filme.id)}>Excluir</button>
             </div>
           </li>
         ))}
